2f3ad421aae709e5fc739a7e68e93ad0a6f644a6
Leo <1224572938@qq.com>
2024-07-26 15:13:30 +0800

    1

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-  },
-  getters: {
+    user: null,
+    activeSubMenu: null
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
+    setActiveSubMenu(state, menu) {
+      state.activeSubMenu = menu;
+    }
   },
   actions: {
+    login({ commit }, user) {
+      // 在这里可以调用后端API
+      commit('setUser', user);
+    },
+    logout({ commit }) {
+      commit('setUser', null);
+    },
+    setActiveSubMenu({ commit }, menu) {
+      commit('setActiveSubMenu', menu);
+    }
   },
-  modules: {
+  getters: {
+    isAuthenticated: state => !!state.user,
+    getUser: state => state.user,
+    getActiveSubMenu: state => state.activeSubMenu
   }
-})
+});
